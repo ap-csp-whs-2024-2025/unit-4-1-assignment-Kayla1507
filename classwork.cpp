e4e076d6957b558d25ab9534eb93c6239ad0b2b4
Kayla1507 <klapham007@gmail.com>
2024-12-12 21:49:30 +0000

    did exercise 1

--- classwork.cpp
+++ classwork.cpp
@@ -3,13 +3,33 @@
 
 int main()
 {
-  int age = 10; //set age to 10
-  double price = 19.99;
-  bool isRaining = false;
-
-  std::cout << age << "\n" << price << std::endl << isRaining << std::endl;
-
-  int grade_level = 11; 
-  std::cout << "I am a " << grade_level << "th grader." << std::endl;
-  return 0;
+  int x;
+  int y;
+  int add_xy;
+  int sub_xy;
+  int x = 25;
+  int y = 3;
+  add_xy = x + y;
+  sub_xy = x - y; 
+  std::cout << add_xy << std::endl
+            << sub_xy << std::endl
+            << melt_xy << std::endl
+            << div_xy << std::endl
+            << mod_xy << std::endl
 }
+
+//exercise 2 
+{
+bool isSunny = true;
+bool hadUmbrella = false;
+std::cout << (isSunny && hasUmbrella) << std::endl;
+
+bool sun_or_umb = isSunny || hasUmbrella; 
+std::cout << sun_or_umb << "\n";
+
+//Exercise 3
+
+
+return 0;
+}
+
